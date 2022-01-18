9cc8c79c3539f39ca6de40a618f6c0a98def7c17
bertogz <bertogzlx@gmail.com>
2022-01-18 07:07:12 -0800

    add item count, move render item to components

--- src/SampleApp.js
+++ src/SampleApp.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { doDetailsExist } from "./Helpers";
 import { useAsyncDataFetchChunk } from "./Hooks/index.js";
 import { getRandomIdsEndpoint, getDetailsEndpoint } from "./Requests";
-
+import {RenderItem} from './Components'
 // This sample app will fetch from a mockDB a list of random ids;
 // once the list of random ids is received;
 // it will be used as param to fetch the details
@@ -32,33 +32,15 @@ function App() {
   };
   // initial call
   useEffect(() => {
-    handleGetDetails(10);
+    handleGetDetails(5);
   }, []);
 
-  // as we get resolved data (details)
-  // we can save this to a state
+  // save what we get from resolvedData (details data)
   useEffect(() => {
     setDetails(resolvedData);
   }, [resolvedData]);
 
-  const RenderItem = ({ item, isLoading, errorLoading }) => {
-    if (errorLoading) {
-      return (
-        <div style={{ backgroundColor: "red" }}>
-          <p>Error Fetching</p>
-        </div>
-      );
-    }
-    if (isLoading) {
-      return (
-        <div style={{ backgroundColor: "yellow" }}>
-          <p>Loading...</p>
-        </div>
-      );
-    } else {
-      return <p>{details.get(item.id)?.label}</p>;
-    }
-  };
+ 
 
   return (
     <div className="App">
@@ -66,21 +48,22 @@ function App() {
       {flatListData.map((item, i) => (
         <RenderItem
           key={item.id + i.toString()}
-          item={item}
+          item={{ ...item, index: i }}
           isLoading={loadingData.has(item.id)}
           errorLoading={errorData.has(item.id)}
+          details={details?.get(item.id)}
         />
       ))}
       <div style={{ alignItems: "center" }}>
         <div
-          style={{ width: 100, backgroundColor: "lightblue" }}
+          style={{margin:10,borderRadius:10, width: 100, backgroundColor: "rgb(100,100,100)" }}
           onClick={() => {
             // get more
-            handleGetDetails(10);
+            handleGetDetails(5);
             console.log("fetching more");
           }}
         >
-          <h4>load more</h4>
+          <h4 style={{color:'white'}}>load more</h4>
         </div>
       </div>
       {doneFetching && <h3>All done fetching!</h3>}
