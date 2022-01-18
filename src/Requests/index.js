3594f6f992f12b5425ca9cc305a68b138383a5f9
bertogz <bertogzlx@gmail.com>
2022-01-17 19:07:57 -0800

    add endpoint

--- src/Requests/index.js
+++ src/Requests/index.js
@@ -1,4 +1,4 @@
-import { detailsDB } from "../MockDB";
+import { primaryDB, detailsDB } from "../MockDB";
 export const getDetailsEndpoint = async (param) => {
   return new Promise((res, rej) => {
     const time = Math.random() * 3000 + 1000;
@@ -10,5 +10,17 @@ export const getDetailsEndpoint = async (param) => {
       debugger;
       res(data);
     }, time);
+  });
+};
+
+export const getRandomIdsEndpoint = (limit) => {
+  return new Promise((res, rej) => {
+    const length = primaryDB.length;
+    let vals = [];
+    for (let i = 0; i < limit; i++) {
+      const rand = Math.floor(Math.random() * length);
+      vals.push(primaryDB[rand]);
+    }
+    res(vals);
   });
 };
