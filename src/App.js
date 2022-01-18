872268cf5deaf663b0dccd636aa84ae4d9403b2d
bertogz <bertogzlx@gmail.com>
2022-01-17 18:17:52 -0800

    continued work on hook

--- src/App.js
+++ src/App.js
@@ -1,55 +1,83 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { useAsyncDataFetch, useAsyncDataFetchChunk } from "./Helpers";
 
-const samplePayload = [
-  { label: "carlos" },
-  { label: "brian" },
-  { label: "Jp" },
-  { label: "Berto" },
-  { label: "Subway" },
-  { label: "Mcdonalds" },
-  { label: "Burger King" },
-  { label: "Wendys" },
+const payload1 = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+  { id: 7 },
+  { id: 8 },
 ];
-const sampleLoadMore = [
-  { label: "Toyota" },
-  { label: "Honda" },
-  { label: "Jeep" },
-  { label: "Volkswagen" },
-  { label: "Ford" },
-  { label: "Chevy" },
-  { label: "Suburu" },
-  { label: "Kia" },
+const payload2 = [
+  { id: 9 },
+  { id: 10 },
+  { id: 11 },
+  { id: 12 },
+  { id: 13 },
+  { id: 14 },
+  { id: 15 },
+  { id: 16 },
 ];
+const randomPayload = [...payload1, ...payload2];
+const details = [
+  { id: 1, label: "carlos" },
+  { id: 2, label: "brian" },
+  { id: 3, label: "Jp" },
+  { id: 4, label: "Berto" },
+  { id: 5, label: "Subway" },
+  { id: 6, label: "Mcdonalds" },
+  { id: 7, label: "Burger King" },
+  { id: 8, label: "Wendys" },
+  { id: 9, label: "Toyota" },
+  { id: 10, label: "Honda" },
+  { id: 11, label: "Jeep" },
+  { id: 12, label: "Volkswagen" },
+  { id: 13, label: "Ford" },
+  { id: 14, label: "Chevy" },
+  { id: 15, label: "Suburu" },
+  { id: 16, label: "Kia" },
+];
+const postRequest = async (param) => {
+  return new Promise((res, rej) => {
+    const time = Math.random() * 3000 + 1000;
+    const data = param.map((item) => details.find((row) => row.id === item.id));
+
+    setTimeout(() => {
+      debugger;
+      res(data);
+    }, time);
+  });
+};
 
 function App() {
-  const [flatListData, setFlatListData] = useState(samplePayload);
-  const [dataToFetch, setDataToFetch] = useState([]);
-
-  useEffect(() => {
-    const missingData = samplePayload.filter((item, i) => {
-      const found = Math.round(Math.random());
-      if (!found) {
-        return true;
-      }
-    });
-    setDataToFetch(missingData);
-    // [{},{}]
-  }, []);
-
-  const postRequest = async () => {
-    return new Promise((res, rej) => {
-      const time = Math.random() * 3000 + 1000;
-      setTimeout(() => res(), time);
-    });
-  };
-
+  const [flatListData, setFlatListData] = useState([]);
+  const [detailsToFetch, setDetailsToFetch] = useState([]);
+  const [details, setDetails] = useState(new Map());
   const { resolvedData, loadingData, doneFetching } = useAsyncDataFetchChunk({
-    oGpayload: dataToFetch,
+    detailsToFetch,
     endpoint: postRequest,
+    keyExtractor: "id",
   });
+
+  const getRandomFlatlistData = (size) => {
+    const length = randomPayload.length;
+    let vals = [];
+    for (let i = 0; i < size; i++) {
+      const rand = Math.floor(Math.random() * length);
+      vals.push(randomPayload[rand]);
+    }
+    return vals;
+  };
+  useEffect(() => {
+    getDetails(getRandomFlatlistData(10));
+  }, []);
+  useEffect(() => {
+    setDetails(resolvedData);
+  }, [resolvedData]);
   const RenderItem = ({ item, isLoading }) => {
     if (isLoading) {
       return (
@@ -58,21 +86,21 @@ function App() {
         </div>
       );
     } else {
-      return <p>{item.label}</p>;
+      return <p>{details.get(item.id)?.label}</p>;
     }
   };
 
-  const getMoreAsyncData = async () => {
+  const getDetails = async (pay) => {
     const response = () => {
       return new Promise((res, rej) => {
         let missingData = [];
         let data = [];
-        sampleLoadMore.map((item, i) => {
-          const found = Math.round(Math.random());
+        pay.map((item) => {
+          const found = details.has(item.id);
           if (!found) {
             missingData.push(item);
           }
-          data.push(item);
+          data.push(item.id, item);
         });
         res({ data, missingData });
       });
@@ -80,9 +108,8 @@ function App() {
     response().then((payload) => {
       // append new payload to flatlistData,
       setFlatListData([...flatListData, ...payload.data]);
-    
-      console.log('fetching more',payload.missingData)
-      setDataToFetch(payload.missingData);
+      console.log("fetching more", payload.missingData);
+      setDetailsToFetch(payload.missingData);
     });
   };
 
@@ -90,14 +117,14 @@ function App() {
     <div className="App">
       <h1>Async Render</h1>
       {flatListData.map((item) => (
-        <RenderItem item={item} isLoading={loadingData.has(item.label)} />
+        <RenderItem item={item} isLoading={loadingData.has(item.id)} />
       ))}
       <div style={{ alignItems: "center" }}>
         <div
           style={{ width: 100, backgroundColor: "lightblue" }}
           onClick={() => {
             // get more, find any missing data to fetch;
-            getMoreAsyncData();
+            getDetails(getRandomFlatlistData(10));
           }}
         >
           <h4>load more</h4>
