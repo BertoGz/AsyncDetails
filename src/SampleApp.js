42c1e04bc4f36c31389587f430f4b9ecee2c6fc4
bertogz <bertogzlx@gmail.com>
2022-01-19 22:53:35 -0800

    add use of json key

--- src/SampleApp.js
+++ src/SampleApp.js
@@ -1,9 +1,9 @@
 import "./SampleApp.css";
 import { useEffect, useState } from "react";
 import { doDetailsExist } from "./Helpers";
-import {useAsyncDataFetch, useAsyncDataFetchChunk } from "./Hooks/index.js";
+import { useAsyncDataFetch, useAsyncDataFetchChunk } from "./Hooks/index.js";
 import { getRandomIdsEndpoint, getDetailsEndpoint } from "./Requests";
-import {RenderItem} from './Components'
+import { RenderItem } from "./Components";
 // This sample app will fetch from a mockDB a list of random ids;
 // once the list of random ids is received;
 // it will be used as param to fetch the details
@@ -14,11 +14,12 @@ function App() {
   const [flatListData, setFlatListData] = useState([]);
   const [detailsToFetch, setDetailsToFetch] = useState([]);
   const [details, setDetails] = useState(new Map());
+
   const { resolvedData, loadingData, errorData, doneFetching } =
     useAsyncDataFetchChunk({
       detailsToFetch,
       endpoint: getDetailsEndpoint,
-      keyExtractor: "id",
+      keyExtractor: ["id"],
     });
 
   const handleGetDetails = (limit) => {
@@ -40,30 +41,36 @@ function App() {
     setDetails(resolvedData);
   }, [resolvedData]);
 
- 
-
   return (
     <div className="App">
       <h1>AsyncDetails</h1>
-      {flatListData.map((item, i) => (
-        <RenderItem
-          key={item.id + i.toString()}
-          item={{ ...item, index: i }}
-          isLoading={loadingData.has(item.id)}
-          errorLoading={errorData.has(item.id)}
-          details={details?.get(item.id)}
-        />
-      ))}
+      {flatListData.map((item, i) => {
+        const idKey = JSON.stringify({ id: item.id });
+        return (
+          <RenderItem
+            key={item.id + i.toString()}
+            item={{ ...item, index: i }}
+            isLoading={loadingData.has(idKey)}
+            errorLoading={errorData.has(idKey)}
+            details={details?.get(idKey)}
+          />
+        );
+      })}
       <div style={{ alignItems: "center" }}>
         <div
-          style={{margin:10,borderRadius:10, width: 100, backgroundColor: "rgb(100,100,100)" }}
+          style={{
+            margin: 10,
+            borderRadius: 10,
+            width: 100,
+            backgroundColor: "rgb(100,100,100)",
+          }}
           onClick={() => {
             // get more
             handleGetDetails(5);
             console.log("fetching more");
           }}
         >
-          <h4 style={{color:'white'}}>load more</h4>
+          <h4 style={{ color: "white" }}>load more</h4>
         </div>
       </div>
       {doneFetching && <h3>All done fetching!</h3>}
