2282fd32fffd5a952cbdb7939ffa2093aa1807ad
bertogz <bertogzlx@gmail.com>
2022-01-19 22:54:10 -0800

    update to use JSON object

--- src/Helpers/index.js
+++ src/Helpers/index.js
@@ -4,10 +4,12 @@
 export const doDetailsExist = async (ids, details) => {
   const response = () => {
     return new Promise((res, rej) => {
+     // let missingDetails = new Map();
       let missingDetails = [];
       ids.map((item) => {
-        const found = details.has(item.id);
+        const found = details.has(JSON.stringify({id:item.id}));
         if (!found) {
+          //missingDetails.set(item.id,item);
           missingDetails.push(item);
         }
       });
